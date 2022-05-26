931d19d5a04e8ecffb5c137c22edf2e5e8fb3d71
tshifhiwa <matombot@gmail.com>
2022-05-26 16:01:57 +0200

    able to add new garments

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.use(express.urlencoded({ extended: false }));
 // const DATABASE_URL = process.env.DATABASE_URL;
 const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/garment_app';
 
-const pool = new pool({
-  connectionString,
-});
+// const pool = new pool({
+//   connectionString,
+// });
 const pgp = PgPromise({});
 const db = pgp(DATABASE_URL);
 
